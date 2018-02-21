214c9876231afbb29f47cd59650e41276996143c
binit Kumar Ambastha <binit.ambastha@gmail.com>
2018-02-21 05:37:18 +0000

    change for if condition

--- sum1.sh
+++ sum1.sh
@@ -1,14 +1,13 @@
-sum = 0
-a = 0
+$b = 10
 echo $n
 for i in $@
 do
-sum=`expr $sum + $i`
-if [[$i -gt 10]]
-then
-echo $i
-a=`expr $sum1 + $i`
-fi
+   sum=`expr $sum + $i`
+   if (($i -gt $b))
+    then
+	echo $i
+        a=`expr $a + $i`
+   fi
 done
 echo "Sum of "$#" no. is : "$sum
 echo "sum of numbers greater than 10 is :"$a
