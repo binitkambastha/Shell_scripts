39a32cb4aec3d78eac75a882eea8d35e3ebf5c22
binit Kumar Ambastha <binit.ambastha@gmail.com>
2018-02-23 04:47:31 +0000

    adding numbers

---
+++ Assignment1.sh
@@ -0,0 +1,16 @@
+$b=10;
+echo $n
+for i in $@
+do
+   sum=`expr $sum + $i`
+   if (("$i" > 10))
+   then
+      echo $i
+      a=`expr $a + $i`
+      echo $a
+   fi
+done
+echo "Sum of "$#" no. is : "$sum
+echo "sum of numbers greater than 10 is :"$a
+
+
